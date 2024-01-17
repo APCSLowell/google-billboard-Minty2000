d7fe4487bcc2b068fe4045c1f85d2967d483a443
Minty2000 <142567186+Minty2000@users.noreply.github.com>
2024-01-17 12:43:12 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -5,9 +5,12 @@ public class GoogleBillboard {
 	public static void main(final String[] args) {
 		  for(int i = 2 ; i < e.length()-10;i++){
   		String digits = e.substring(i, 10+i);
-	  double dNum = Double.parseDouble(digits);
+	  double dNum = Double.parseDouble(digits
+					  if(isPrime(dNum)== true){
+  return( digits );  //displays 7.182818284E9
+  }
 		  }
-		return (isPrime(dNum));
+		
 	
 	
 	//Finish this function
